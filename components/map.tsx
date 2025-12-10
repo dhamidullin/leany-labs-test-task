44b4cf2654dab872f0c5a08202a260ea7caf5466
Danis Khamidullin <danis.zp.123@gmail.com>
2025-12-10 10:00:52 +0200

    Added basic map component

---
+++ components/map.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { useEffect, useRef } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+export default function Map() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<maplibregl.Map | null>(null);
+
+  useEffect(() => {
+    if (map.current) return; // stops map from initializing more than once
+
+    if (mapContainer.current) {
+      map.current = new maplibregl.Map({
+        container: mapContainer.current,
+        style: {
+          version: 8,
+          sources: {
+            'osm-tiles': {
+              type: 'raster',
+              tiles: [
+                'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
+              ],
+              tileSize: 256,
+              attribution: '&copy; OpenStreetMap Contributors',
+            }
+          },
+          layers: [
+            {
+              id: 'osm-tiles',
+              type: 'raster',
+              source: 'osm-tiles',
+              minzoom: 0,
+              maxzoom: 19
+            }
+          ]
+        },
+        center: [0, 0],
+        zoom: 1
+      });
+    }
+  }, []);
+
+  return <div className="w-full h-screen" ref={mapContainer} />;
+}
